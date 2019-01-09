db46be722e4a84cea8792645a07185fdc3cbfe0c
Claus Bo Hansen <sbo@trolling.dk>
2019-01-09 13:46:42 +0100

    Killing existing instance

--- StartService.R
+++ StartService.R
@@ -1,3 +1,12 @@
+
+#### Load libraries ####
 library(Rserve)
+
+#### Initiate server ####
+
+# Kill any existing instance of Rserve
+system("killall -INT Rserve")
+
+# Start Rserve
 Rserve(args = "--no-save")
 
