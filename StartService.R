e07e799a9f48db00ac9b2f01cb5339ea40098d5f
Claus Bo Hansen <claus.bo.hansen@garmin.com>
2019-01-10 13:48:35 +0100

    Added check for OS when killing existing proces

--- StartService.R
+++ StartService.R
@@ -1,11 +1,16 @@
 
+# Determine OS
+OS <- Sys.info()["sysname"]
+
 #### Load libraries ####
 library(Rserve)
 
 #### Initiate server ####
 
-# Kill any existing instance of Rserve
-system("killall -INT Rserve")
+# If on Linux, Kill any existing instance of Rserve
+if (OS == "Linux"){
+  system("killall -INT Rserve")
+}
 
 # Start Rserve
 Rserve(args = "--no-save")
