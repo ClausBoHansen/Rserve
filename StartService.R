61bc975c1a0de65d21114b89380875091712c8bd
Claus Bo Hansen <sbo@trolling.dk>
2019-01-24 17:16:37 +0100

    Removed test function

--- StartService.R
+++ StartService.R
@@ -4,24 +4,24 @@
 # Install Rserve
 # install.packages('Rserve',,'http://www.rforge.net/')
 
-# Test function
-hello <- function(name) {
-  paste("Hi ", name,", how are you?", sep = "")
-}
-
-
-# Determine OS
-OS <- Sys.info()["sysname"]
 
 #### Load libraries ####
 library(Rserve)
+library(RserveFunctions)
+
+# Load library with Machine Learning function
+#library(testpackage)
+
+
+load("finalModel.rf.RData")
+
+
+# Call Prediction example
+# Prediction(TrainedModel, 12)
+
 
 #### Initiate server ####
-
-# If on Linux, Kill any existing instance of Rserve
-if (OS == "Linux"){
-  system("killall -INT Rserve")
-}
+system("killall -INT Rserve")
 
 # Start Rserve
 Rserve(args = "--no-save")
